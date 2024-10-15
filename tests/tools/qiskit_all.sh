7857d2f01dc6d125518fe34af78d6b69d415f7ba
kawashima <ryoya.kawashima@metro.co.jp>
2024-10-15 11:04:52 +0900

    Fixed Qiskit script: Don't delete files even if no errors occur

--- tests/tools/qiskit_all.sh
+++ tests/tools/qiskit_all.sh
@@ -7,7 +7,7 @@ PARALLEL_JOBS=10
 # Specify the Python script to run
 SCRIPT="qiskit_1to1.py"
 
-function processIRFile() {
+function toQiskit() {
     file="$1"
     output_file="${file%_IR.txt}_output.log"
     error_file="${file%_IR.txt}_error.log"
@@ -15,17 +15,13 @@ function processIRFile() {
     echo "Target file: $file, Output file: $output_file, Error file: $error_file"
 
     python3 "$SCRIPT" "$file" > "$output_file" 2> "$error_file"
-
-    if [ ! -s "$error_file" ]; then
-        rm "$error_file"
-    fi
 }
 
 cd $(dirname "$0")
 
-export -f processIRFile
+export -f toQiskit
 export SCRIPT
 
-find "$TARGET_DIR" -name '*_IR.txt' | xargs -P "$PARALLEL_JOBS" -I% bash -c 'processIRFile %'
+find "$TARGET_DIR" -name '*_IR.txt' | xargs -P "$PARALLEL_JOBS" -I% bash -c 'toQiskit %'
 
 echo "All processes have completed."
